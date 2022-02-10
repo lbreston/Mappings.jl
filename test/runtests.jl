dff6f1e6c2072066a363ed4aa7fed6d09059efec
Leo Breston <leo.breston@gmail.com>
2022-02-10 05:29:41 -0800

    cleaned up sets removed insert need to add back

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,14 @@
 using Mappings
 using Test
 
-D1=Dict([("a"=>"1"), ("b"=>"3"), ("c"=>"3")])
-m=AssociativeMap(D1)
-insert!(m, ("d", "4"))
+D1 = Dict([("a" => "1"), ("b" => "3"), ("c" => "3")])|>x->Dict{keytype(x), valtype(x)}(x)
+m = AssociativeMap(D1)
+inverse(m)
+
+
+
 
 @testset "Mappings.jl" begin
     # Write your tests here.
 end
+
