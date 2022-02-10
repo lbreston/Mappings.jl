53c3bed6d942788ca1d5dfb03831618f5a89fac8
Leo Breston <leo.breston@gmail.com>
2022-02-09 23:37:24 -0800

    reorganized

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using Mappings
 using Test
 
+D1=Dict([("a"=>"1"), ("b"=>"3"), ("c"=>"3")])
+m=AssociativeMap(D1)
+insert!(m, ("d", "4"))
+
 @testset "Mappings.jl" begin
     # Write your tests here.
 end
