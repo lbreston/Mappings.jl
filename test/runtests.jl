bd44bf1f37a457698a93d95f71f35fad28af3980
Leo Breston <leo.breston@gmail.com>
2022-02-10 09:04:45 -0800

    no longer a function

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,17 @@
 using Mappings
 using Test
 
-D1 = Dict([("a" => "1"), ("b" => "3"), ("c" => "3")])|>x->Dict{keytype(x), valtype(x)}(x)
+D1 = Dict([(x, rand(1000)) for x in 'A':'Z'])
 m = AssociativeMap(D1)
 inverse(m)
+
+
+
+using BenchmarkTools
+m.(Set(('A', 'B', 'C')))
+Set(m.(['A', 'B', 'C']))
+
+@benchmark (collect(D1))
 
 
 
